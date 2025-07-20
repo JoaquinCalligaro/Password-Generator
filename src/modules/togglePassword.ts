f9448969994f646a233ee6a9aa8bf88cf055d251
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-07-20 11:09:16 -0300

    a test checkbox has been added to fix a bug.

--- src/modules/togglePassword.ts
+++ src/modules/togglePassword.ts
@@ -1,24 +1,12 @@
-function setupTogglePassword(passwordInput: HTMLInputElement) {
-  // Obtiene el botón de mostrar/ocultar contraseña.
-const togglePasswordButton = document.getElementById("togglePassword");
-// Agrega un listener al botón de mostrar/ocultar contraseña.
-togglePasswordButton?.addEventListener("click", () => {
-  // Obtiene el tipo actual del campo de entrada de la contraseña.
-  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
-  // Establece el nuevo tipo del campo de entrada de la contraseña.
-  passwordInput.setAttribute("type", type);
+window.addEventListener("DOMContentLoaded", () => {
+  const passwordInput = document.getElementById("password") as HTMLInputElement | null;
+  const toggleCheckbox = document.getElementById("togglePasswordCheckbox") as HTMLInputElement | null;
 
-  // Obtiene las imágenes de ojo abierto y cerrado.
-  const eyeOpen = togglePasswordButton.querySelector('img[src="./assets/icons/eye-open.png"]');
-  const eyeClosed = togglePasswordButton.querySelector('img[src="./assets/icons/closed-eye.png"]');
-
-  // Alterna la visibilidad de las imágenes de ojo abierto y cerrado.
-  if (type === "password") {
-    eyeOpen?.classList.remove("hidden");
-    eyeClosed?.classList.add("hidden");
+  if (passwordInput && toggleCheckbox) {
+    toggleCheckbox.addEventListener("change", () => {
+      passwordInput.type = toggleCheckbox.checked ? "text" : "password";
+    });
   } else {
-    eyeOpen?.classList.add("hidden");
-    eyeClosed?.classList.remove("hidden");
+    console.error("No se encontraron elementos passwordInput o toggleCheckbox");
   }
 });
-}
